9a5e9113e44775d783f17578eb613a03fa5a776b
Hector984 <hectoruam96@gmail.com>
2025-01-23 07:48:49 -0600

    Creating user page component

--- projects/github-finder/src/App.jsx
+++ projects/github-finder/src/App.jsx
@@ -7,6 +7,8 @@ import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import { GithubProvider } from "./context/github/GithubContext";
 import { AlertProvider } from "./context/alert/AlertContext";
+import Alert from "./components/layout/Alert";
+import User from "./pages/User";
 
 function App() {
   return (
@@ -17,9 +19,11 @@ function App() {
             <Navbar title="Github Finder" />
 
             <main className="container mx-auto px-3 pb-12">
+              <Alert/>
               <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
+                <Route path="/user/:login" element={<User />} />
                 <Route path="/notfound" element={<NotFound />} />
                 <Route path="/*" element={<NotFound />} />
               </Routes>
--- projects/github-finder/src/context/github/GithubContext.jsx
+++ projects/github-finder/src/context/github/GithubContext.jsx
@@ -7,17 +7,16 @@ const GITHUB_TOKEN = import.meta.env.VITE_REACT_APP_GITHUB_TOKEN;
 const GITHUB_URL = import.meta.env.VITE_REACT_APP_GITHUB_URL;
 
 export const GithubProvider = ({ children }) => {
-
   const initialState = {
     users: [],
-    loading: false
-  }
+    user: {},
+    loading: false,
+  };
 
-  const [state, dispatch] = useReducer(githubReducer, initialState)
+  const [state, dispatch] = useReducer(githubReducer, initialState);
 
   // This function fetches users from the Github API, but it is only for testting purposes
   const fetchUsers = async () => {
-
     setLoading();
     const results = await fetch(`${GITHUB_URL}/users`, {
       headers: {
@@ -30,48 +29,78 @@ export const GithubProvider = ({ children }) => {
     dispatch({
       type: "GET_USERS",
       payload: data,
-    })
-
-  }
+    });
+  };
 
   const searchUsers = async (text) => {
     setLoading();
 
     const params = new URLSearchParams({
-      q: text
+      q: text,
     });
 
     const results = await fetch(`${GITHUB_URL}/search/users?${params}`, {
       headers: {
         Authorization: `token ${GITHUB_TOKEN}`,
-      }
+      },
     });
 
-    const {items} = await results.json();
+    const { items } = await results.json();
 
     dispatch({
       type: "SEARCH_USERS",
       payload: items,
     });
+  };
 
-  }
+  const getUser = async (login) => {
+    setLoading();
+
+    const results = await fetch(`${GITHUB_URL}/users/${login}`, {
+      headers: {
+        Authorization: `token ${GITHUB_TOKEN}`,
+      },
+    });
+
+    if (results.status === 404) {
+      window.location.href = "/notfound";
+    } else {
+      const data = await results.json();
+
+      dispatch({
+        type: "GET_USER",
+        payload: data,
+      });
+    }
+  };
 
   const clearUsers = () => {
     dispatch({
-      type: "CLEAR_USERS"
-    })
-  }
+      type: "CLEAR_USERS",
+    });
+  };
 
   const setLoading = () => {
     dispatch({
       type: "SET_LOADING",
-    })
-  }
+    });
+  };
 
-  return <GithubContext.Provider value={ 
-    {users: state.users, loading: state.loading, fetchUsers, searchUsers, clearUsers}}>
-    {children}
-  </GithubContext.Provider>
+  return (
+    <GithubContext.Provider
+      value={{
+        users: state.users,
+        loading: state.loading,
+        fetchUsers,
+        searchUsers,
+        clearUsers,
+        user: state.user,
+        getUser,
+      }}
+    >
+      {children}
+    </GithubContext.Provider>
+  );
 };
 
 export default GithubContext;
