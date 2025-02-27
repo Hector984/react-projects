3328aae6b41b024c942506dfcf3676328304b34b
Hector984 <hectoruam96@gmail.com>
2025-02-27 07:37:24 -0600

    Finish registrstion with google

--- projects/house-market-place-2/src/components/OAuth.jsx
+++ projects/house-market-place-2/src/components/OAuth.jsx
@@ -1,6 +1,6 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-import { doc, setDoc, getDoc } from "firebase/firestore";
+import { doc, setDoc, getDoc, serverTimestamp } from "firebase/firestore";
 import { db } from "../firebase.config";
 import { toast } from "react-toastify";
 import googleIcon from '../assets/svg/googleIcon.svg'
@@ -15,8 +15,27 @@ const OAuth = () => {
             const auth = getAuth();
             const provider = new GoogleAuthProvider();
             const result = await signInWithPopup(auth, provider);
-        }catch(error) {
 
+            // We get the user from the google sign in
+            const user = result.user;
+
+            // Check for user in the db
+            const docRef = doc(db, 'users', user.uid);
+            const docSnap = await getDoc(docRef);
+
+            // If user does not exists then we create it in the db
+            if(!docSnap.exists()) {
+                await setDoc(doc(db, 'users', user.uid), {
+                    name: user.displayName,
+                    email: user.email,
+                    timestamp: serverTimestamp()
+                })
+            }
+
+            navigate('/');
+
+        }catch(error) {
+            toast.error('Could not authorize with google');
         }
     }
 
