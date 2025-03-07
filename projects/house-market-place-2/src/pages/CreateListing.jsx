4e87450ee7edaacf4ceaf8091370efd85be8eaaa
Hector984 <hectoruam96@gmail.com>
2025-03-07 08:04:26 -0600

    Implementing the create listing component

--- projects/house-market-place-2/src/pages/CreateListing.jsx
+++ projects/house-market-place-2/src/pages/CreateListing.jsx
@@ -1,56 +1,147 @@
 import { useState, useEffect, useRef } from "react";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
-import Spinner  from '../components/Spinner';
+import Spinner from "../components/Spinner";
 
 const CreateListing = () => {
+  const [geolocationEnabled, setGeolocationEnabled] = useState(true);
+  const [loading, setLoading] = useState(false);
 
-    const [geolocationEnabled, setGeolocationEnabled] = useState(true);
-    const [loading, setLoading] = useState(false);
+  const [formData, setFormData] = useState({
+    type: "rent",
+    name: "",
+    bedrooms: 1,
+    bathrooms: 1,
+    parking: false,
+    furnished: false,
+    address: "",
+    offer: false,
+    regularPrice: 0,
+    discountedPrice: 0,
+    images: {},
+    latitude: 0,
+    longitude: 0,
+  });
 
-    const [formData, setFormData] = useState({
-        type: 'rent',
-        name: '',
-        bedrooms: 1,
-        bathrooms: 1,
-        parking: false,
-        furnished: false,
-        address: '',
-        affer: false,
-        regularPrice: 0,
-        discountedPrice: 0,
-        images: {},
-        latitude: 0,
-        longitude: 0
-    });
+  const {
+    type,
+    name,
+    bedrooms,
+    bathrooms,
+    parking,
+    furnished,
+    address,
+    offer,
+    regularPrice,
+    discountedPrice,
+    images,
+    latitude,
+    longitude,
+  } = formData;
+  const auth = getAuth();
+  const navigate = useNavigate();
+  const isMounted = useRef(true);
 
-    const auth = getAuth();
-    const navigate = useNavigate();
-    const isMounted = useRef(true);
-
-    useEffect(() => {
-
-        if(isMounted) {
-            onAuthStateChanged(auth, (user) => {
-                if(user) {
-                    setFormData({...formData, userRef: user.uid});
-                } else {
-                    navigate('/sign-in')
-                }
-            })
+  useEffect(() => {
+    if (isMounted) {
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          setFormData({ ...formData, userRef: user.uid });
+        } else {
+          navigate("/sign-in");
         }
-
-        return () => {
-            isMounted.current = false;
-        }
-
-    }, [isMounted]);
-
-    if(loading) {
-        return <Spinner/>
+      });
     }
 
-    return <div>Create</div>
-}
+    return () => {
+      isMounted.current = false;
+    };
+  }, [isMounted]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const onMutate = (e) => {
+    setFormData((prevState) => ({
+        ...prevState,
+        [e.target.id]: e.target.value
+    }))
+  };
+
+  if (loading) {
+    return <Spinner />;
+  }
+
+  return (
+    <div className="profile">
+      <header>
+        <p className="pageHeader">Create Listing</p>
+      </header>
+
+      <main>
+        <form onSubmit={handleSubmit}>
+          <label className="formLabel">Sell / Rent</label>
+          <div className="formButtons">
+            <button
+              type="button"
+              className={type === "sale" ? "formButtonActive" : "formButton"}
+              id="type"
+              value="sale"
+              onClick={onMutate}
+            >
+              Sell
+            </button>
+
+            <button
+              type="button"
+              className={type === "rent" ? "formButtonActive" : "formButton"}
+              id="rent"
+              value="rent"
+              onClick={onMutate}
+            >
+              Rent
+            </button>
+          </div>
+
+          <label className="formLabel">Name</label>
+          <input type="text" id="name" 
+          className="formInputName"
+          value={name}
+          onChange={onMutate}
+          maxLength="32"
+          minLength="10"
+          required          
+          />
+
+          <div className="formRooms flex">
+            <div>
+                <label className="formLabel">Bedrooms</label>
+                <input type="number" className="formInputSmall"
+                id="bedrooms"
+                value={bedrooms}
+                onChange={onMutate}
+                min="1"
+                max="50"
+                required
+                />
+            </div>
+            <div>
+                <label className="formLabel">Bathrooms</label>
+                <input type="number" className="formInputSmall"
+                id="bathrooms"
+                value={bathrooms}
+                onChange={onMutate}
+                min="1"
+                max="50"
+                required
+                />
+            </div>
+          </div>
+        </form>
+      </main>
+    </div>
+  );
+};
 
 export default CreateListing;
